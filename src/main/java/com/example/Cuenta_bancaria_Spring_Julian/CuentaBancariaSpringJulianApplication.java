45102c632907f8c2c5ef2d5feedb2ad0a8795f7c
JAgamez21 <zjulian1208@gmail.com>
2025-01-02 11:39:46 -0500

    feat: Inicializar el proyecto de cuenta bancaria con la configuración básica

---
+++ src/main/java/com/example/Cuenta_bancaria_Spring_Julian/CuentaBancariaSpringJulianApplication.java
@@ -0,0 +1,13 @@
+package com.example.Cuenta_bancaria_Spring_Julian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuentaBancariaSpringJulianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuentaBancariaSpringJulianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Cuenta_bancaria_Spring_Julian/CuentaBancariaSpringJulianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Cuenta_bancaria_Spring_Julian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuentaBancariaSpringJulianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
